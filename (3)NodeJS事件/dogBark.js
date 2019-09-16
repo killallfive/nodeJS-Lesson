39ba68706c82936474e1095cb04ec806ea9651e6
killallfive <869104815@qq.com>
2019-09-16 14:02:39 +0800

    homework

--- (3)NodeJS事件/dogBark.js
+++ (3)NodeJS事件/dogBark.js
@@ -5,18 +5,20 @@ const dog = require("./dog");
 var dog1 = new dog.Dog("taidi", 4);
 var dog2 = new dog.Dog("zangao", 8);
 dog1.on("bark", () => {
-    if (dog1.energy >= 0) {
+    if (dog1.energy > 0) {
         console.log(dog1.name + " barked! energy:" + dog1.energy);
         dog1.energy--;
     } else {
+        console.log(dog1.name + " barked! energy:" + dog1.energy);
         process.exit();
     }
 });
 dog2.on("bark", () => {
-    if (dog2.energy >= 0) {
+    if (dog2.energy > 0) {
         console.log(dog2.name + " barked! energy:" + dog2.energy);
         dog2.energy--;
     } else {
+        console.log(dog2.name + " barked! energy:" + dog2.energy);
         process.exit();
     }
 });
