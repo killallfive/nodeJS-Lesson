449c10a589c6924e1a986baa56a8d9574d6600ad
killallfive <869104815@qq.com>
2019-09-16 22:21:46 +0800

    homework

---
+++ (4)NodeJS流/streamServer.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+http.createServer(function(req, res) {
+    var filePath = path.join(__dirname, "/data.txt");
+    var streamRead = fs.createReadStream(filePath);
+    streamRead.pipe(res);
+}).listen(8081);
+console.log("server is listening 8081");
