e0220df343069438454b512ebb80a9ddd4a33a6e
killallfive <869104815@qq.com>
2019-09-18 10:41:04 +0800

    homework

--- (5)NodeJS文件系统操作/fileReader1.js
+++ (5)NodeJS文件系统操作/fileReader1.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-// var filePath = path.join(__dirname, "/file.txt");
+var fileName = process.argv[2];
+
 http.createServer(function(req, res) {
-    if(process.argv[2] == undefined) {
+    /**
+     * fs.readFile() 是一个异步方法，执行到该句
+     * 不会等待文件读取完成，就执行后面的语句
+     * 回调函数是等到文件读取完成之后才执行
+     */
+    if(fileName == undefined) {
         fs.readFile(__filename, function(err, data) {
             if(err) {
                 res.write(err);
@@ -14,15 +20,21 @@ http.createServer(function(req, res) {
             }
         })
     } else {
-        var filePath = path.join(__dirname, "/" + process.argv[2])
-        fs.readFile(filePath, function(err, data) {
-            if(err) {
-                res.write("The path doesn't exist");
-                res.end();
-            } else {
-                res.write(data.toString());
-                res.end();
-            }
-        })
+        var filePath = path.join(__dirname, fileName);
+        if(fs.existsSync(filePath)) {
+            fs.readFile(filePath, function(err, data) {
+                if(err) {
+                    res.end(err);
+                } else {
+                    res.end(data.toString());
+                }
+            })
+        } else {
+            res.writeHead(200,{"Content-Type": "text/html;charset=utf-8"});
+            res.end("该文件不存在");
+            // res.end("this file doesn't exist");
+        }
+
     }
 }).listen(8081);
+console.log("server is listening 8081");
